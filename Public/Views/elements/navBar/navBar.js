3285f5210578f1bdbda67e848131243e3a99c8ab
Francisco448 <sisco_j_M@hotmail.com.ar>
2023-01-24 05:06:17 -0300

    new changes

---
+++ Public/Views/elements/navBar/navBar.js
@@ -0,0 +1,33 @@
+// require('Jquery');
+
+var sidebar = $('.sidebar');
+var header = $('.header');
+var button = $('.header > button');
+var item_list = $('.item_list');
+var li = $('.item_list > li');
+var a = $('.item_list > li > a');
+var i = $('.item_list > li > a > i');
+var span = $('.item_list > li > a > span');
+
+
+button.on('click', function(){
+    if(sidebar.hasClass('open')){
+        sidebar.removeClass('open');
+        header.removeClass('open');
+        button.removeClass('open');
+        item_list.removeClass('open');
+        li.removeClass('open');
+        a.removeClass('open');
+        i.removeClass('open');
+        span.removeClass('open');
+    }else{
+        sidebar.addClass('open');
+        header.addClass('open');
+        button.addClass('open');
+        item_list.addClass('open');
+        li.addClass('open');
+        a.addClass('open');
+        i.addClass('open');
+        span.addClass('open');
+    }
+})
