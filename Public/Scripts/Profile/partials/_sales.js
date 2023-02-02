74b365d78f446f8a901d9119643ebb2950ceb009
Francisco448 <sisco_j_M@hotmail.com.ar>
2023-02-02 13:57:28 -0300

    working in sales

--- Public/Scripts/Profile/partials/_sales.js
+++ Public/Scripts/Profile/partials/_sales.js
@@ -22,13 +22,27 @@ $(document).ready(function () {
     });
 });
 
+
 $(document).ready(function () {
+    $('.addProduct').on('click', function () {
+        if ($('#DataGridStock').DataTable().row('tr.selected').data() != undefined) {
+            $('#DataGridSale').DataTable().row.add({
+                "Name": $('#DataGridStock').DataTable().row('tr.selected').data().Name,
+                "SalePrice": $('#DataGridStock').DataTable().row('tr.selected').data().SalePrice,
+                "Units": $('#addUnits').val()
+            }).draw(false);
+        }
+    })
     $('#DataGridSale').DataTable({
         "columns": [
-            { "data": "Name" },
-            { "data": "SalePrice" },
-            { "data": "Units" }
+            { "data": "Name", "name": "Name", "autoWidth": true },
+            { "data": "SalePrice", "name": "SalePrice", "autoWidth": true },
+            { "data": "Units", "name": "Units", "autoWidth": true }
         ],
+        "columnDefs": [{
+            "defaultContent": "-",
+            "targets": "_all"
+        }],
         "paging": false,
     });
     $('#DataGridSale tbody').on('click', 'tr', function () {
